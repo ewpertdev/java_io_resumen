4b206b09b172f1c10aa923fbd041099786d4b1cb
ewpertdev <xblackcat93@gmail.com>
2024-09-25 14:11:10 +0200

    Buffered write method

--- src/HolaBea/LeerFichero.java
+++ src/HolaBea/LeerFichero.java
@@ -7,13 +7,13 @@ public class LeerFichero {
         try {
             FileReader salida = new FileReader(fichero);
             BufferedReader buffer = new BufferedReader(salida);
-            String texto="";
+            String texto = "";
 
 //            while((texto=buffer.readLine()!=null)){
 //                System.out.println(texto);
 //            }
-            while(texto!=null){
-                texto=buffer.readLine();
+            while (texto != null) {
+                texto = buffer.readLine();
                 System.out.println(texto);
             }
 
@@ -22,45 +22,64 @@ public class LeerFichero {
         }
     }
 
-    public static void Escritura (String ruta){
-        try{
+    // Non-buffered method
+    public static void Escritura(String ruta) {
+        try {
             FileWriter fichero = new FileWriter(ruta);
             PrintWriter pw = new PrintWriter(fichero);
 
-            for(int i=0; i<10;i++) {
-                pw.write("numero \n"+"\n"+i);
+            for (int i = 0; i < 10; i++) {
+                pw.write(" numero \n" + "\n" + i);
             }
             fichero.close();
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.getStackTrace();
         }
-
-
     }
-            public static void main (String[]args){
-                // String ruta="\"";
-                String ruta = "Fichero.txt";
-                File fichero = new File(ruta);
+
+    // Buffered method
+    public static void Escritura2(String ruta) {
+        try {
+            FileWriter fichero = new FileWriter(ruta, true);
+            BufferedWriter bw = new BufferedWriter(fichero);
+
+            for (int i = 22; i < 30; i++) {
+                bw.write(" " + i);
+                bw.newLine();
+            }
+            bw.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public static void main(String[] args) {
+        // String ruta="\"";
+        String ruta = "Fichero.txt";
+        File fichero = new File(ruta);
 
 
-                if (fichero.exists()) {
-                    System.out.println("Leyendo..");
-                    // Llamamos a la función de lectura
+        if (fichero.exists()) {
+            System.out.println("Leyendo..");
+            // Llamamos a la función de lectura
 //                    Lectura(fichero);
-                    LecturaBuffer(fichero);
-                    Escritura(ruta);
+            LecturaBuffer(fichero);
+            Escritura(ruta);
+            Escritura2(ruta);
 
-                } else {
-                    System.out.println("no existe el fichero" + fichero.getAbsolutePath());
-                    try {
-                        if (fichero.createNewFile()) {
-                            System.out.println("Fichero creado: " + fichero.getAbsolutePath());
+        } else {
+            System.out.println("no existe el fichero" + fichero.getAbsolutePath());
+            try {
+                if (fichero.createNewFile()) {
+                    System.out.println("Fichero creado: " + fichero.getAbsolutePath());
 
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+    }
+}
 
